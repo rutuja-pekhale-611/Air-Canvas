2e58f145cd096e5d3c9b8c878de37628f30fe73a
Rutuja <129038011+rutuja-pekhale-611@users.noreply.github.com>
2024-02-22 21:16:34 +0530

    Update air_canvas.py

--- air_canvas.py
+++ air_canvas.py
@@ -2,12 +2,12 @@ import numpy as np
 import cv2
 from collections import deque
 
-#default called trackbar function 
+ 
 def setValues(x):
    print("")
 
 
-# Creating the trackbars needed for adjusting the marker colour
+ 
 cv2.namedWindow("Color detectors")
 cv2.createTrackbar("Upper Hue", "Color detectors", 153, 180, setValues)
 cv2.createTrackbar("Upper Saturation", "Color detectors", 255, 255, setValues)
@@ -17,7 +17,7 @@ cv2.createTrackbar("Lower Saturation", "Color detectors", 72, 255, setValues)
 cv2.createTrackbar("Lower Value", "Color detectors", 49, 255, setValues)
 
 
-# Giving different arrays to handle colour points of different colour
+ 
 bpoints = [deque(maxlen=1024)]
 gpoints = [deque(maxlen=1024)]
 rpoints = [deque(maxlen=1024)]
